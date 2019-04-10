99c761266a7c301ce729299a64298d34afd84da9
José Oliveira <josecarlosdeoliveirajr@gmail.com>
2019-04-10 17:03:16 -0300

    Get all tests.

--- kayle.py
+++ kayle.py
@@ -3,9 +3,26 @@ import subprocess
 import shlex
 
 
-def main():
+def get_tests():
     inputs_file_names = os.listdir("inputs")
-    inputs_file_names = os.listdir("outputs")
+    outputs_file_names = os.listdir("outputs")
+    if len(inputs_file_names) != len(outputs_file_names):
+        raise Exception("Number os inputs and outputs is not equal.")
+    tests = []
+    for i in range(len(inputs_file_names)):
+        input_file = open("inputs/" + inputs_file_names[i], "r")
+        output_file = open("outputs/" + outputs_file_names[i], "r")
+        input_content = input_file.read()
+        output_content = output_file.read()
+        tests.append({
+            "name": inputs_file_names[i],
+            "input": input_content,
+            "output": output_content
+        })
+    return tests
+
+
+def main():
     # output = os.system("python submissions/submission.py < inputs/input1.txt")
     # print(output)
     command = "python submissions/submission.py"
@@ -22,6 +39,9 @@ def main():
 
     print("Outs", outs.decode("utf-8"))
     print("Errs", errs)
+
+    tests = get_tests()
+    print(tests)
 
 
 if __name__ == "__main__":
